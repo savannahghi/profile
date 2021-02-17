e1a36234e05ed817b527654bbffd70dde729a468
Vincent Michuki <vincentmichuki@savannahinformatics.com>
2021-02-17 11:24:26 +0300

    add ci test run

--- test/sil_contacts_test.dart
+++ test/sil_contacts_test.dart
@@ -1,1 +1,65 @@
+import 'package:async_redux/async_redux.dart';
+import 'package:built_collection/built_collection.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sil_contacts/contact_items_card.dart';
+import 'package:sil_contacts/contact_utils.dart';
+import 'package:sil_contacts/sil_contacts.dart';
 
+void main() {
+  group('ContactDetails', () {
+    testWidgets('renders 4 ContactItemsCard widgets if primaryEmail is defined',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: ContactProvider(
+              primaryEmail: 'someone@example.com',
+              primaryPhone: '+254728101710',
+              secondaryEmails: BuiltList([]),
+              secondaryPhones: BuiltList([]),
+              contactUtils: ContactUtils(
+                toggleLoadingIndicator: null,
+                client: null,
+                updateStateFunc: null,
+              ),
+              wait: Wait(),
+              checkWaitingFor: () {},
+              child: ContactDetails(),
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byType(ContactItemsCard), findsNWidgets(4));
+      expect(find.byType(ContactDetails), findsOneWidget);
+    });
+    testWidgets(
+        'renders 3 ContactItemsCard widgets if primaryEmail is not defined',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: ContactProvider(
+              primaryEmail: 'UNKNOWN',
+              primaryPhone: '+254728101710',
+              secondaryEmails: BuiltList([]),
+              secondaryPhones: BuiltList([]),
+              contactUtils: ContactUtils(
+                toggleLoadingIndicator: null,
+                client: null,
+                updateStateFunc: null,
+              ),
+              wait: Wait(),
+              checkWaitingFor: () {},
+              child: ContactDetails(),
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byType(ContactItemsCard), findsNWidgets(3));
+      expect(find.byType(ContactDetails), findsOneWidget);
+    });
+  });
+}
